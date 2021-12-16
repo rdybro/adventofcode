970ecd7efbae327cbf8889ce5c9a44cc698a7f0d
Rasmus Dybro Larsen <rasmus@dybro.net>
2021-12-16 17:31:00 +0100

    2021 day 15 part 1 update

--- 2021/day15/part1.ps1
+++ 2021/day15/part1.ps1
@@ -3,16 +3,16 @@
 # Rasmus Dybro Larsen
 # Day 15 - Part 1
 
-$inputContent = Get-Content -LiteralPath input.txt
-#$inputContent = Get-Content -LiteralPath input_example.txt
+$inputReader = New-Object System.IO.StreamReader("$((Get-Location).Path)\input.txt")
+#$inputReader = New-Object System.IO.StreamReader("$((Get-Location).Path)\input_example.txt")
 
 $sizeVertical = -1
 $sizeHorizontal = -1
 $locationIndex = -1
 
-$cave = [System.Collections.Generic.List[PSCustomObject]]::new()
+$cave = @{}
 
-foreach($line in $inputContent) {
+while($line = $inputReader.ReadLine()) {
     
     $sizeVertical++
     $sizeHorizontal = -1
@@ -22,51 +22,75 @@ foreach($line in $inputContent) {
         $sizeHorizontal++
         $locationIndex++
 
-        $cave.Add([PSCustomObject]@{
+        $cave["$sizeVertical-$sizeHorizontal"] = @{
             Index = $locationIndex
             Vertical = $sizeVertical
             Horizontal = $sizeHorizontal
+            LocationString = "$sizeVertical-$sizeHorizontal"
             Risk = if($locationIndex -eq 0) { 0 } else { [System.Int32]::Parse($char) }
             Total = if($locationIndex -eq 0) { 0 } else { 2147483647 }
             Visited = $false
-        })
+        }
     }
 }
 
-while($cave | Where-Object { $_.Visited -eq $false }) {
+$inputReader.Close()
+$inputReader.Dispose()
 
-    $currentLocation = $cave | Where-Object { ($_.Visited -eq $false) -and ($_.Total -lt 2147483647) } | Sort-Object Total | Select-Object -First 1
+$queue = [System.Collections.Generic.List[string]]@("0-0")
 
-    $locationDown = $cave | Where-Object { ($_.Vertical -eq $currentLocation.Vertical + 1) -and ($_.Horizontal -eq $currentLocation.Horizontal) }
-    $locationRight = $cave | Where-Object { ($_.Vertical -eq $currentLocation.Vertical) -and ($_.Horizontal -eq $currentLocation.Horizontal + 1) }
+while($queue) {
 
-    $locationUp = $cave | Where-Object { ($_.Vertical -eq $currentLocation.Vertical - 1) -and ($_.Horizontal -eq $currentLocation.Horizontal) }
-    $locationLeft = $cave | Where-Object { ($_.Vertical -eq $currentLocation.Vertical) -and ($_.Horizontal -eq $currentLocation.Horizontal - 1) }
+    $nextPosition = $null
+    $nextPositionTotalRisk = 2147483647
+    
+    foreach($item in $queue) {
+        
+        $queueItem = $cave[$item]
+        
+        if($queueItem.Total -lt $nextPositionTotalRisk) {
+            
+            $nextPosition = $queueItem.LocationString
+            $nextPositionTotalRisk = $cave[$item].Total
+        }
+    }
+
+    $currentLocation = $cave[$nextPosition]
+    
+    $locationDown = $cave["$($currentLocation.Vertical + 1)-$($currentLocation.Horizontal)"]
+    $locationRight = $cave["$($currentLocation.Vertical)-$($currentLocation.Horizontal + 1)"]
+
+    $locationUp = $cave["$($currentLocation.Vertical - 1)-$($currentLocation.Horizontal)"]
+    $locationLeft = $cave["$($currentLocation.Vertical)-$($currentLocation.Horizontal - 1)"]
 
     if($locationDown -and ($currentLocation.Total + $locationDown.Risk -lt $locationDown.Total) -and ($locationDown.Visited -eq $false)) {
 
         $locationDown.Total = $currentLocation.Total + $locationDown.Risk
+        $queue.Add($locationDown.LocationString)
     }
 
     if($locationRight -and ($currentLocation.Total + $locationRight.Risk -lt $locationRight.Total) -and ($locationRight.Visited -eq $false)) {
         
         $locationRight.Total = $currentLocation.Total + $locationRight.Risk
+        $queue.Add($locationRight.LocationString)
     }
 
     if($locationUp -and ($currentLocation.Total + $locationUp.Risk -lt $locationUp.Total) -and ($locationUp.Visited -eq $false)) {
 
         $locationUp.Total = $currentLocation.Total + $locationUp.Risk
+        $queue.Add($locationUp.LocationString)
     }
 
     if($locationLeft -and ($currentLocation.Total + $locationLeft.Risk -lt $locationLeft.Total) -and ($locationLeft.Visited -eq $false)) {
 
         $locationLeft.Total = $currentLocation.Total + $locationLeft.Risk
+        $queue.Add($locationLeft.LocationString)
     }
 
     $currentLocation.Visited = $true
+    $null = $queue.Remove($nextPosition)
 }
 
-Write-Host "The lowest total risk of any path from the top left to the bottom right is $(($cave[$locationIndex]).Total)."
+Write-Host "The lowest total risk of any path from the top left to the bottom right is $($cave["$sizeVertical-$sizeHorizontal"].Total)."
 
 # Answer is 498
-# This took 2h5m to run
