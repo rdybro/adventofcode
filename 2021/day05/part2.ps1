0d6ae232ceb14e793b9dc0c47e0b5c15662e7a4f
Rasmus Dybro Larsen <rasmus@dybro.net>
2021-12-23 17:50:15 +0100

    2021 day 5 part 2 solution

--- 2021/day05/part2.ps1
+++ 2021/day05/part2.ps1
@@ -1,7 +1,7 @@
 
 # Advent of Code 2021
 # Rasmus Dybro Larsen
-# Day 5 - Part 1
+# Day 5 - Part 2
 
 $inputReader = New-Object System.IO.StreamReader("$((Get-Location).Path)\input.txt")
 #$inputReader = New-Object System.IO.StreamReader("$((Get-Location).Path)\input_example.txt")
@@ -13,12 +13,23 @@ while($line = $inputReader.ReadLine()) {
     $coordinates = $line.Split("->").Trim().Split(",")
 
     if($coordinates[0] -eq $coordinates[2]) {
-        
+               
         foreach($point in @($coordinates[1]..$coordinates[3])) { $pointsCovered.Add("$($coordinates[0]),$point") }
     }
     elseif($coordinates[1] -eq $coordinates[3]) {
 
         foreach($point in @($coordinates[0]..$coordinates[2])) { $pointsCovered.Add("$point,$($coordinates[1])") }
+    }
+    elseif((($coordinates[0] - $coordinates[2]) -eq ($coordinates[1] - $coordinates[3])) -or (($coordinates[0] - $coordinates[2]) -eq ($coordinates[3] - $coordinates[1]))) {
+
+        $array = @($coordinates[1]..$coordinates[3])
+        $arrayIndex = 0
+
+        foreach($point in @($coordinates[0]..$coordinates[2])) {
+            
+            $pointsCovered.Add("$point,$($array[$arrayIndex])")
+            $arrayIndex++
+        }
     }
 }
 
@@ -27,4 +38,4 @@ $inputReader.Dispose()
 
 Write-Host "There is $(($pointsCovered | Group-Object | Where-Object { $_.Count -gt 1 }).Count) points where at least 2 lines overlaps."
 
-# Answer is 7085
+# Answer is 20271
