f8ddf287ea45355cafdeb725e53bb6d989ff1ea3
Rasmus Dybro Larsen <rasmus@dybro.net>
2022-12-05 21:40:20 +0100

    2022 day 5 solutions

---
+++ 2022/day05/part2.ps1
@@ -0,0 +1,55 @@
+
+# Advent of Code 2022
+# Rasmus Dybro Larsen
+# Day 5 - Part 2
+
+$inputReader = New-Object System.IO.StreamReader("$((Get-Location).Path)\input.txt")
+#$inputReader = New-Object System.IO.StreamReader("$((Get-Location).Path)\input_example.txt")
+
+$stacks = $null
+
+while($line = $inputReader.ReadLine()) {
+
+    if($line.Substring(0,2) -ne " 1") {
+        
+        $stackTotal = ($line.Length + 1) / 4
+    
+        if($null -eq $stacks) {
+            
+            $stacks = @{}
+            for($i = 1; $i -le $stackTotal; $i++) { $stacks.Add($i,[System.Collections.Generic.List[string]]::new()) }
+        }
+    
+        $lineIndex = 1
+    
+        for($i = 1; $i -le $stackTotal; $i++) {
+            
+            $lineSubstring = $line.Substring($lineIndex,1)
+            if($lineSubstring -ne " ") { $stacks[$i].Add($lineSubstring) }
+            $lineIndex += 4
+        }
+    }
+}
+
+while($line = $inputReader.ReadLine()) {
+
+    $lineSplit = $line.Split(" ")
+    $moveFrom = [int]$lineSplit[3]
+    $moveTo = [int]$lineSplit[5]
+
+    for($i = $lineSplit[1] - 1; $i -ge 0; $i--) {
+
+        $stacks[$moveTo].Insert(0,$stacks[$moveFrom][$i])
+        $stacks[$moveFrom].RemoveAt($i)
+    }
+}
+
+$inputReader.Close()
+$inputReader.Dispose()
+
+$answer = ""
+foreach($stack in $stacks.Keys | Sort-Object) { $answer += $stacks[$stack][0] }
+
+Write-Host "The crates on top of each stack is $answer."
+
+# Answer is RNRGDNFQG
